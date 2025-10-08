95785a9646ce0a3e4b7682f0e25c0f6f35135716
Bangar Parth Manik <106005954+ParthBangar@users.noreply.github.com>
2025-10-08 23:35:51 +0530

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -32,7 +32,7 @@ const NavBar = () => {
                 </div>
 
                 <div className='hidden lg:block'>
-                    <a className='bg-[#00df72] px-8 py-4 text-xl rounded-md' href="https://drive.google.com/file/d/14p5f4kGTFKy24LMRU3rkgjQ3Caxvzq0e/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                    <a className='bg-[#00df72] px-8 py-4 text-xl rounded-md' href="https://drive.google.com/file/d/1XrKNuktTWjcALteVSBzK3VWT651LDCbN/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                         Resume
                     </a>
                 </div>
@@ -59,7 +59,7 @@ const NavBar = () => {
 
                         <NavLink className="text-xl cursor-pointer" to="/home#contacts"><span className={`hover:border-b-2 hover:border-[#00df72] ${location.hash === '#contacts' ? "border-b-2 border-[#00df72]" : " "}`}>./contact</span></NavLink>
 
-                        <a className='bg-[#00df72] px-4 py-1 text-xl rounded-md text-center' href="https://drive.google.com/file/d/14p5f4kGTFKy24LMRU3rkgjQ3Caxvzq0e/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+                        <a className='bg-[#00df72] px-4 py-1 text-xl rounded-md text-center' href="https://drive.google.com/file/d/1XrKNuktTWjcALteVSBzK3VWT651LDCbN/view?usp=sharing" target="_blank" rel="noopener noreferrer">
                             Resume
                         </a>
                     </div>
